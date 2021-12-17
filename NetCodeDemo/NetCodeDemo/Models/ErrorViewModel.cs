276ce1f3ce26852011160aecfde9aa5be13073a4
Lance <lance@rb2.nl>
2021-12-17 15:43:48 +0800

    Create a new project

---
+++ NetCodeDemo/NetCodeDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCodeDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
